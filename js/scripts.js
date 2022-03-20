7ab10f8d9026a5dfe01470356e9be63485ef740d
Shalyne-Wangari <shalyne.waweru@student.moringaschool.com>
2022-03-21 00:23:30 +0300

    Display the order in the order summary table

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
   //On Clicking the Order Button
   $("#btn-order").click(function(){
-    //Get the Selected Pizza Size,Topping and Crust
+    //Get the Selected Pizza Size,Toppings and Crust
     let pizzaSize = $("#size option:selected").text();
     let pizzaToppings = $("#toppings option:selected").text();
     let pizzaCrust = $("#crust option:selected").text();
@@ -17,7 +17,11 @@ $(document).ready(function(){
     let pizzaCrustPrize = parseInt($("#crust option:selected").val());
     let total = pizzaSizePrize + pizzaToppingsPrize + pizzaCrustPrize;
 
-    console.log(total);
+    //Set the selected order details in the order summary
+    $("#pizzaOrderSize").html(pizzaSize);
+    $("#pizzaOrderToppings").html(pizzaToppings);
+    $("#pizzaOrderCrust").html(pizzaCrust);
+    $("#totalOrderCost").html(total);
   });
 
 });
